5b42f9f188ec762186154743974430def156b622
ezypolis.senapahlevi <sena.pahlevi@ezypolis.com>
2023-06-02 21:10:34 +0700

    using calculate distance knn

--- go.mod
+++ go.mod
@@ -13,6 +13,14 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang-basic/golearn v0.0.0-20141106095017-ec846e62d2ef // indirect
+	github.com/gonum/blas v0.0.0-20181208220705-f22b278b28ac // indirect
+	github.com/gonum/floats v0.0.0-20181209220543-c233463c7e82 // indirect
+	github.com/gonum/internal v0.0.0-20181124074243-f884aa714029 // indirect
+	github.com/gonum/lapack v0.0.0-20181123203213-e4cdc5a0bff9 // indirect
+	github.com/gonum/matrix v0.0.0-20181209220409-c518dec07be9 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/guptarohit/asciigraph v0.5.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.3.1 // indirect
@@ -22,16 +30,23 @@ require (
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.7 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/olekukonko/tablewriter v0.0.4 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/rocketlaunchr/dataframe-go v0.0.0-20201007021539-67b046771f0b // indirect
+	github.com/sjwhitworth/golearn v0.0.0-20221228163002-74ae077eafb2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/exp v0.0.0-20200331195152-e8c3332aa8e5 // indirect
 	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	gonum.org/v1/gonum v0.8.1 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/postgres v1.5.2 // indirect
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -13,7 +15,7 @@ import (
 )
 
 type House struct {
-	ID        int       `gorm:"id"`
+	ID        uint      `gorm:"id"`
 	Tipe      string    `gorm:"tipe"`
 	Alamat    string    `gorm:"alamat"`
 	Lat       string    `gorm:"lat"`
@@ -23,35 +25,105 @@ type House struct {
 	DeletedAt time.Time `gorm:"deleted_at"`
 }
 
-// func (house *House) BeforeSave(tx *gorm.DB) (err error) {
-// 	long, err := strconv.ParseFloat(house.Long, 64)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	lat, err := strconv.ParseFloat(house.Lat, 64)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	house.Long = strconv.FormatFloat(long, 'f', -1, 64)
-// 	house.Lat = strconv.FormatFloat(lat, 'f', -1, 64)
-// 	return nil
-// }
+type Calculate struct {
+	OriginID        int       `gorm:"originid"`
+	DestinationID   int       `gorm:"destinationid"`
+	LatOrigin       string    `gorm:"lat_origin"`
+	LongOrigin      string    `gorm:"long_origin"`
+	LatDestination  string    `gorm:"lat_destination"`
+	LongDestination string    `gorm:"long_destination"`
+	CreatedAt       time.Time `gorm:"created_at"`
+	UpdatedAt       time.Time `gorm:"updated_at"`
+}
+type RouteDistance struct {
+	Distance int `json:"distance"`
+}
 
-// func createRumah(db *gorm.DB) error {
-// 	rumah := House{
-// 		Tipe:   "Tipe Rumah A",
-// 		Alamat: "Alamat Rumah",
-// 		Lat:    123.456,
-// 		Long:   789.012,
-// 	}
+type Route struct {
+	Houses   []House `json:"houses"`
+	Distance float64 `json:"distance"`
+}
 
-// 	err := db.Create(&rumah).Error
-// 	if err != nil {
-// 		return err
-// 	}
+func calculateDistance(house1, house2 House) float64 {
 
-// 	return nil
-// }
+	lat1, _ := strconv.ParseFloat(house1.Lat, 64)
+	long1, _ := strconv.ParseFloat(house1.Long, 64)
+	lat2, _ := strconv.ParseFloat(house2.Lat, 64)
+	long2, _ := strconv.ParseFloat(house2.Long, 64)
+
+	return math.Sqrt(math.Pow(lat2-lat1, 2) + math.Pow(long2-long1, 2))
+}
+
+// Nearest Neighbor algorithm to calculate the optimum route
+func calculateOptimumRoute(houses []House) Route {
+	var route Route
+	var visited = make(map[uint]bool)
+
+	// Start from the first house
+	currentHouse := houses[0]
+	route.Houses = append(route.Houses, currentHouse)
+	visited[currentHouse.ID] = true
+
+	// Visit the remaining houses
+	for len(route.Houses) < len(houses) {
+		var nearestHouse House
+		var minDistance float64
+
+		// Find the nearest unvisited house
+		for _, house := range houses {
+			if !visited[house.ID] {
+				distance := calculateDistance(currentHouse, house)
+				if minDistance == 0 || distance < minDistance {
+					minDistance = distance
+					nearestHouse = house
+				}
+			}
+		}
+
+		// Visit the nearest house
+		route.Houses = append(route.Houses, nearestHouse)
+		visited[nearestHouse.ID] = true
+		currentHouse = nearestHouse
+	}
+
+	// Calculate the total distance of the route
+	for i := 0; i < len(route.Houses)-1; i++ {
+		route.Distance += calculateDistance(route.Houses[i], route.Houses[i+1])
+	}
+
+	// Return the calculated route
+	return route
+}
+
+// Define a constant for the radius of the earth in kilometers
+const earthRadius = 6371.0
+
+// Convert degrees to radians
+func deg2rad(deg float64) float64 {
+	return deg * math.Pi / 180.0
+}
+
+// Calculate the haversine distance between two points in kilometers
+func haversineDistance(originLong float64, originLat float64, destinationLong float64, destinationLat float64) float64 {
+
+	// Convert the latitude and longitude values to radians
+	originLat = deg2rad(originLat)
+	originLong = deg2rad(originLong)
+	destinationLat = deg2rad(destinationLat)
+	destinationLong = deg2rad(destinationLong)
+
+	// Calculate the differences between the latitude and longitude values
+	dLat := destinationLat - originLat
+	dLon := destinationLong - originLong
+
+	// Apply the haversine formula
+	a := math.Pow(math.Sin(dLat/2), 2) + math.Cos(originLat)*math.Cos(destinationLat)*math.Pow(math.Sin(dLon/2), 2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	d := earthRadius * c
+	fmt.Println("hello ini KM haversineDistance", d)
+	// Return the distance in kilometers
+	return d
+}
 
 func main() {
 	dsn := "host=localhost user=postgres password=123456789 dbname=address-house-map-bf port=5432 sslmode=disable TimeZone=Asia/Jakarta"
@@ -62,7 +134,7 @@ func main() {
 	}
 
 	fmt.Println("berhasil konek")
-	err = db.AutoMigrate(&House{})
+	err = db.AutoMigrate(&Calculate{})
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
@@ -73,16 +145,9 @@ func main() {
 
 	router := gin.Default()
 
-	// config := cors.DefaultConfig()
-	// config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
-	// config.AllowHeaders = []string{"Content-Type"}
-	// router.Use(cors.New(config))
-
 	router.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"http://localhost:3000"},
 		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
-		// AllowHeaders:     []string{"Origin"},
-		// ExposeHeaders:    []string{"Content-Type"},
 		AllowHeaders: []string{"Content-Type"},
 		// ExposeHeaders:    []string{"Content-Type"},
 		AllowCredentials: true,
@@ -130,7 +195,7 @@ func main() {
 
 		c.JSON(http.StatusOK, house)
 	})
-	api.PUT("/house/:id", func(c *gin.Context) {
+	api.PUT("/house-update/:id", func(c *gin.Context) {
 		var house House
 		id := c.Param("id")
 		// Pencarian data berdasarkan ID
@@ -173,23 +238,65 @@ func main() {
 		}
 		c.JSON(http.StatusOK, house)
 	})
+	type Location struct {
+		Lat string `json:"lat"`
+		Lon string `json:"long"`
+	}
 
 	api.POST("/calculate-route", func(c *gin.Context) {
-		// var house House
-		var house House
-		id := c.Param("id")
-		result := db.Model(&house).Where("id = ?", id).First(&house)
-		if result.Error != nil {
-			c.JSON(http.StatusNotFound, gin.H{"error": "house not found"})
+
+		//coba start
+
+		// var locations []Location
+		// err := c.ShouldBindJSON(&locations)
+		// if err != nil {
+		// 	c.JSON(http.StatusBadRequest, gin.H{"error bind": err.Error()})
+		// 	return
+		// }
+
+		// var houses []House
+		// for _, loc := range locations {
+		// 	houses = append(houses, House{Lat: loc.Lat, Long: loc.Lon})
+		// }
+
+		// // Calculate the optimum route using the Nearest Neighbor algorithm
+		// route := calculateOptimumRoute(houses)
+
+		//coba end
+
+		var dataOriginHouse House
+		var dataDestinationHouse House
+		var calculate Calculate
+
+		err := c.ShouldBindJSON(&calculate)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		originData := db.Where("id = ?", calculate.OriginID).First(&dataOriginHouse)
+
+		fmt.Println("calcuate origin lat", dataOriginHouse.Lat)
+		// id := c.Param("id")
+		if originData.Error != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Data Origin not found"})
 			return
 		}
 
-		result = db.Delete(&house)
-		if result.Error != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		destinationData := db.Where("id = ?", calculate.DestinationID).First(&dataDestinationHouse)
+
+		fmt.Println("originData", originData)
+		if destinationData.Error != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error or=": destinationData.Error.Error()})
 			return
 		}
-		c.JSON(http.StatusOK, house)
+
+		// originLat, _ := strconv.ParseFloat(dataOriginHouse.Lat, 64)
+		// originLong, _ := strconv.ParseFloat(dataOriginHouse.Long, 64)
+		// destinationLat, _ := strconv.ParseFloat(dataDestinationHouse.Lat, 64)
+		// destinationLong, _ := strconv.ParseFloat(dataDestinationHouse.Long, 64)
+
+		c.JSON(http.StatusOK, calculate)
+		// c.JSON(http.StatusOK, route)
 	})
 	router.Run(":8080")
 }
